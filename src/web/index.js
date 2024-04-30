a0e181fa82bc413529f67a8a599ab631eccc24ad
albertnis <4309984+albertnis@users.noreply.github.com>
2024-04-30 18:32:29 +1200

    Properly encode server public key in client

--- src/web/index.js
+++ src/web/index.js
@@ -1,6 +1,26 @@
 const SERVER_PUBLIC_KEY =
   'BCUdAv5I5pJwseflX848zH2IQHwaSsvMRcE-7Exhq-ES1HssHg_UnHeinOlN9rg3dLrdSPd0IFfMvK08Ixj8e0w'
 
+/**
+ *
+ * @param {string} base64String
+ * @returns
+ */
+function urlB64ToUint8Array(base64String) {
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
+  const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/')
+
+  const rawData = window.atob(base64)
+  const outputArray = new Uint8Array(rawData.length)
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
+}
+
+const applicationServerKey = urlB64ToUint8Array(SERVER_PUBLIC_KEY)
+
 const tryParseJson = (input) => {
   try {
     return JSON.parse(input)
@@ -29,8 +49,8 @@ const registerPermission = async () => {
   const registration = await navigator.serviceWorker.ready
 
   const pushSubscription = await registration.pushManager.subscribe({
-    applicationServerKey: SERVER_PUBLIC_KEY,
-    userVisibleOnly: false,
+    applicationServerKey,
+    userVisibleOnly: true,
   })
 
   const response = await fetch('/api/pushSubscription', {
